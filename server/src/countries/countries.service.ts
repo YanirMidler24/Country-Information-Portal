6e1561cce89b51f4be6137184d7ccd7bed0ed610
yanir midler <yanir.midler@walla.net.il>
2024-07-09 09:26:59 +0300

    update

--- server/src/countries/countries.service.ts
+++ server/src/countries/countries.service.ts
@@ -19,42 +19,36 @@ export class CountriesService {
   ) {}
 
   async findAll(): Promise<Country[]> {
+    this.logger.log('Fetching all countries');
     try {
       const countries = await this.countryModel
         .find()
         .collation({ locale: 'en', strength: 1 })
         .sort({ countryName: 1 })
         .exec();
-
-      this.logger.log('Fetching all countries');
-
+      this.logger.log('Successfully fetched all countries');
       return countries;
     } catch (error) {
       this.logger.error('Failed to fetch all countries', { error });
-
       throw new InternalServerErrorException('Failed to fetch all countries');
     }
   }
 
   async findOne(id: string): Promise<Country> {
+    this.logger.log(`Fetching country with id: ${id}`);
     try {
       const country = await this.countryModel.findById(id).exec();
-
       if (!country) {
         this.logger.warn(`Country with id ${id} not found`);
         throw new NotFoundException(`Country with id ${id} not found`);
       }
-
-      this.logger.log(`Fetching country with id: ${id}`);
-
+      this.logger.log(`Successfully fetched country with id: ${id}`);
       return country;
     } catch (error) {
       this.logger.error(`Failed to fetch country with id: ${id}`, { error });
-
       if (error instanceof NotFoundException) {
         throw error;
       }
-
       throw new InternalServerErrorException('Failed to fetch country');
     }
   }
@@ -63,26 +57,22 @@ export class CountriesService {
     id: string,
     updateCountryDto: UpdateCountryDto,
   ): Promise<Country> {
+    this.logger.log(`Updating country with id: ${id}`);
     try {
       const updatedCountry = await this.countryModel
         .findByIdAndUpdate(id, updateCountryDto, { new: true })
         .exec();
-
       if (!updatedCountry) {
         this.logger.warn(`Country with id ${id} not found`);
         throw new NotFoundException(`Country with id ${id} not found`);
       }
-
-      this.logger.log(`Updating country with id: ${id}`);
-
+      this.logger.log(`Successfully updated country with id: ${id}`);
       return updatedCountry;
     } catch (error) {
       this.logger.error(`Failed to update country with id: ${id}`, { error });
-
       if (error instanceof NotFoundException) {
         throw error;
       }
-
       throw new InternalServerErrorException('Failed to update country');
     }
   }
